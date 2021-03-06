0fb53996206226b552240cb4072d468b94239632
archie125 <crbeliber@gmail.com>
2021-03-06 13:29:12 -0800

    BELIBER_CRIS ARCHIE: Search Component Functionality

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-import { Input, Button, Select } from 'antd';
+import { Input, Select, Button } from 'antd';
 
 import './search.scss';
 
 const {Option} = Select;
 
 class Search extends Component {
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
     render() {
         return ( 
             <div className='search-component'>
-                <form className='search-component-form'> 
+                <form className='search-component-form' 
+                onSubmit={(event) => this.onFormSubmit(event)}
+                > 
                     <div>
                         <Input
                             name='query'
                             className='search'
                             size='large'
                             placeholder='Search for (e.g Hardware)'
+                            value={this.props.query}
+                            onChange={(event) => this.props.onChange(event)}
                         />
                     </div>
                     <div>
-                    <div className='search-option'>
-                        <Input.Group compact>
-                            <Input name='distance' />
-                        <Select defaultValue='Distance'>
-                            <Option value='Distance'>Distance (km)</Option>
-                            <Option value='Current location'>Current location</Option>
-                        </Select>
-                        <Button
-                            className='btn-search'
-                            type='primary'
-                            shape='round'
-                            size='meduim'
-                            htmlType='submit'
-                            >
-                                SEARCH
-                        </Button>
-                        </Input.Group>
+                        <div className='search-option'>
+                            <Input.Group compact>
+                                <Input 
+                                name='distance' 
+                                value={this.props.distance} 
+                                onChange={(event) => this.props.onChange(event)}/>
+                            <Select defaultValue='Distance'>
+                                <Option value='Distance'>Distance (km)</Option>
+                                <Option value='Current location'>Current location</Option>
+                            </Select>
+                            <Button
+                                className='btn-search'
+                                type='primary'
+                                shape='round'
+                                size='meduim'
+                                htmlType='submit'
+                                >
+                                    SEARCH
+                            </Button>
+                            </Input.Group>
+                        </div>
                     </div>
-                </div>
                 </form>
             </div>
             );
